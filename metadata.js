31850268d580e48dae71c76b27e33d09209f3208
Silas Reinagel <silas.reinagel@gmail.com>
2024-08-21 21:26:35 -0500

    Update OG Image URL

--- metadata.js
+++ metadata.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'A Simple Fix - NWA Handyman - Bella Vista, Bentonville, Rogers, Lowell, Pea Ridge',
   url: 'https://asimplefixnwa.com/',
-  image: 'https://asimplefixnwa.com/images/opengraph.jpg',
+  image: 'https://asimplefixnwa.com/opengraph.jpg',
   description: 'A Simple Fix is your trusted handyman service in NWA, offering home repair and maintenance services in Bella Vista, Bentonville, Rogers, Lowell, and Pea Ridge.',
   keywords: 'handyman, Bella Vista, Bentonville, Rogers, Lowell, Pea Ridge, home repair, maintenance, NWA'
 };
