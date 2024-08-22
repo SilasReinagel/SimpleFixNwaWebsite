833fe52447263e4a6b6c53760a9d0c72180b5fb2
Silas Reinagel <silas.reinagel@gmail.com>
2024-08-21 21:36:53 -0500

    Event Tracking

--- src/Elements/EmailLink.svelte
+++ src/Elements/EmailLink.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="center-h bottom">
-  <a class="row" href="mailto:{company.email}">
+  <a class="row" href="mailto:{company.email}" plausible-event-name="Begin Email">
     <div class="email-icon">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor" aria-label="Email Icon">
         <path d="M20 4H4C2.9 4 2 4.9 2 6v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
--- src/Elements/PhoneNumberLink.svelte
+++ src/Elements/PhoneNumberLink.svelte
@@ -1,9 +1,10 @@
 <script>
   import company from '../static-content.js';
+
 </script>
 
 <div class="center-h bottom">
-  <a class="row" href="tel:{company.phone}">
+  <a class="row" href="tel:{company.phone}" plausible-event-name="Call Phone">
     <div class="phone-icon">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor" aria-label="Phone Icon">
         <path d="M6.62 10.79a15.053 15.053 0 006.59 6.59l2.2-2.2a1 1 0 011.11-.27c1.21.49 2.53.76 3.88.76a1 1 0 011 1v3.5a1 1 0 01-1 1C10.29 22 2 13.71 2 3.5a1 1 0 011-1H6.5a1 1 0 011 1c0 1.35.27 2.67.76 3.88a1 1 0 01-.27 1.11l-2.2 2.2z"/>
@@ -16,7 +17,6 @@
 </div>
 
 <style>
-
   .bottom {
     height: 32px;
   }
